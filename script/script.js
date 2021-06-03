6a037133a86d9b239e7487d3f5bcf684dfb8af9e
Katerina <loginovakaty@gmail.com>
2021-06-03 12:47:27 +0300

    lesson event and DOM

---
+++ script/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const btnAdder = document.getElementById('button');
+const newToDo = document.getElementById('todo');
+const listToDo = document.getElementById('todolist');
+
+
+btnAdder.addEventListener('click', handlerAddToDo);
+
+function handlerAddToDo() {
+    if (!isEmpty(newToDo.value)) {
+        const item = createItemOfList(newToDo.value);
+        addItemToList(item);
+    }
+
+    newToDo.value = '';
+}
+
+function isEmpty(str) {
+    return Number(str) === 0;
+}
+
+function createItemOfList(contentItem) {
+    const item = document.createElement('li');
+
+    item.classList.add('list__item');
+    item.textContent = contentItem;
+
+    return item;
+}
+
+function addItemToList(item) {
+    listToDo.append(item);
+}
